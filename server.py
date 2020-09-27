007924fef2618edfe24a7587c43b122a9185159c
Ján Macek <macek.1991@gmail.com>
2020-09-27 13:10:08 +0200

    feat: connection between flask and html is done

--- server.py
+++ server.py
@@ -1,25 +1,31 @@
-from flask import Flask
-from flask import Flask, render_template,session, request
+from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return render_template('hate-speech-detector.html')
-
-
-@app.route('/design1')
-def design1():
-    return render_template('design_1.html')
-
-
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
 def process():
-    text = request.form['text']
+    return render_template('index.html')
 
-    print(text)
-    return render_template('hate-speech-detector.html')
+
+@app.route('/api/v1/check', methods=['GET'])
+def api_check():
+    if 'message' in request.args:
+        message = request.args['message']
+    else:
+        return jsonify({
+            'result': -1,
+            'msg': 'No message provided. Please specify a message to be checked.'
+        }), 422
+
+    # No hate speech = 0 / Hate speech = 1
+    result = 1
+
+    return jsonify({
+        'result': result,
+        'msg': 'Message is ' + ('' if result == 1 else 'not ') + 'a hate speech.'
+    }), 200
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=88)
+    app.run(host='localhost', port=88)
